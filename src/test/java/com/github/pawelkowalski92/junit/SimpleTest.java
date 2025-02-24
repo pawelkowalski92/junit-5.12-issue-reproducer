80fc9121af690565effdfdb1a1601a04edf22a12
Paweł Kowalski <pawel.a.kowalski@allegro.com>
2025-02-24 13:17:57 +0100

    Add simple test

---
+++ src/test/java/com/github/pawelkowalski92/junit/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.github.pawelkowalski92.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void shouldRun() {
+        Assertions.assertTrue(true);
+    }
+
+}
